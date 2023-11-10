bccfa027f83f18e5795d678bd71ad604125263a3
Adrián <contrerasbuenoadrian@gmail.com>
2023-11-10 10:10:55 +0100

    Todos los cálculos hechos

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         boolean datosIntroducidos = false;
         boolean calculoRealizado = false;
         int eleccion;
+        float total = 0;
+        float viajero1 = 0, viajero2 = 0, viajero3 = 0, viajero4 = 0, viajero5 = 0;
+        int numeroViajeros = 5;
+        float media=0;
 
         do {
             System.out.println("\nMenú:");
@@ -15,13 +19,11 @@ public class Main {
             System.out.println("4. Salir");
             System.out.print("Elige una opción: ");
             eleccion = scanner.nextInt();
-            float viajero1 = 0, viajero2 = 0, viajero3 = 0, viajero4 = 0, viajero5 = 0;
-            int numeroViajeros = 5;
-            float total = 0;
+
 
             if (eleccion == 1) {
-                datosIntroducidos = true;                                                                       // si viajero > media --> viajero - media --> le deben
-                                                                                                                // si viajero < media --> media - viajero --> moroso
+                datosIntroducidos = true;
+
                 // lógica para introducir los gastos
                 System.out.println("¿Cuanto tiene que pagar el viajero 1?");
                 viajero1 = scanner.nextFloat();
@@ -49,7 +51,8 @@ public class Main {
                 }
 
                 total=viajero1+viajero2+viajero3+viajero4+viajero5;
-                System.out.println(total +" "+numeroViajeros);
+                media=total/numeroViajeros;
+                System.out.println("El total es: "+ total);
 
                 System.out.println("Gastos de los viajeros introducidos.");
             } else if (eleccion == 2) {
@@ -57,14 +60,51 @@ public class Main {
                     System.out.println("Primero debes ejecutar la opción 1.");
                 } else {
                     calculoRealizado = true;
-                    // lógica para calcular el reparto de gastos
+
                     System.out.println("Reparto de gastos calculado.");
+                    System.out.println("Cada viajero debe de pagar "+media);
                 }
             } else if (eleccion == 3) {
                 if (!calculoRealizado) {
                     System.out.println("Primero debes ejecutar la opción 2.");
                 } else {
-                    // lógica para mostrar el reparto de gastos
+                    if (viajero1>media) {
+                        viajero1=viajero1-media;
+                        System.out.println("Al viajero 1 le deben "+viajero1);
+                    }else {
+                        viajero1=media-viajero1;
+                        System.out.println("El viajero 1 debe "+viajero1);
+                    }
+                    if (viajero2>media) {
+                        viajero2=viajero2-media;
+                        System.out.println("Al viajero 2 le deben "+viajero2);
+                    }else {
+                        viajero2=media-viajero2;
+                        System.out.println("El viajero 2 debe "+viajero2);
+                    }
+                    if (viajero3>media&&viajero1>0) {
+                        viajero3=viajero3-media;
+                        System.out.println("Al viajero 3 le deben "+viajero3);
+                    }else if (viajero3<media&&viajero3>0){
+                        viajero3=media-viajero3;
+                        System.out.println("El viajero 3 debe "+viajero3);
+                    }
+                    if (viajero4>media&&viajero4>0) {
+                        viajero4=viajero4-media;
+                        System.out.println("Al viajero 4 le deben "+viajero4);
+                    }else if (viajero4<media&&viajero4>0) {
+                        viajero4=media-viajero4;
+                        System.out.println("El viajero 4 debe "+viajero4);
+                    }
+                    if (viajero5>media&&viajero5>0) {
+                        viajero5=viajero5-media;
+                        System.out.println("Al viajero 5 le deben "+viajero5);
+                    }else if (viajero5<media&&viajero5>0) {
+                        viajero5=media-viajero5;
+                        System.out.println("El viajero 5 debe "+viajero5);
+                    }
+
+
                     System.out.println("Reparto de gastos mostrado.");
                 }
             } else if (eleccion == 4) {
